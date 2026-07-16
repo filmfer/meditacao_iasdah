fd0df8060becee4d151f0bd619233bd8a14d0e2c
J. Fillipe Fernandes <filmfer@gmail.com>
2026-07-16 10:03:04 +0000

    Update send_whatsapp.js

--- send_whatsapp.js
+++ send_whatsapp.js
@@ -17,6 +17,11 @@ const client = new Client({
             '--disable-gpu'
         ]
     }
+    // Add this webVersionCache block:
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+    }
 });
 
 client.on('qr', (qr) => {
